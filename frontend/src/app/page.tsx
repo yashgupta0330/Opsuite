b8ebe8f5e758dd3db16d1a69197376e22564f0ac
charu.maheshwari <yash.gupta@anarish.com>
2026-04-20 16:27:36 +0530

    Removed your project is starting page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,25 @@
-import ProjectRunning from "@/components/ProjectRunning";
+import HeroPage from "./pages/Homepage/hero";
+import StatsSection from "./pages/Homepage/stats";
+import BrandPage from "./pages/Homepage/brand";
+import CustomerServiceTabs from "./pages/Homepage/customerservice";
+import IndustriesSection from "./pages/Homepage/industriesection";
+import WhyServitium from "./pages/Homepage/whyservitium";
+import MobilityAppsSection from "./pages/Homepage/mobility";
+import AutomationSection from "./pages/Homepage/smartautomation";
+import TestimonialsSection from "./pages/Homepage/testimonials";
 
 export default function Home() {
-  return <ProjectRunning />;
+  return (
+    <>
+      <HeroPage />
+      <BrandPage />
+      <WhyServitium />
+      <StatsSection />
+      <IndustriesSection />
+      <CustomerServiceTabs />
+      <MobilityAppsSection />
+      <AutomationSection />
+      <TestimonialsSection />
+    </>
+  );
 }
